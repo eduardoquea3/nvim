e65af06d4fc384faf0928edb8bac2b1d6b3d311e
Eduardo Quea <eduardoquea3@gmail.com>
2024-09-18 20:54:12 -0500

    feat: Refactoring of localization of some plugins based on file type

---
+++ lua/plugins/language/lua.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/lazydev.nvim",
+  config = true,
+  ft = "lua",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+  },
+}
---
+++ lua/plugins/language/yaml.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "b0o/schemastore.nvim",
+    ft = { "yaml" },
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("lspconfig").yamlls.setup {
+        settings = {
+          yaml = {
+            schemas = require("schemastore").yaml.schemas(),
+          },
+        },
+      }
+    end,
+  },
+}
