d5f8eb191b580909619390493299ead99dbe3294
Eduardo Quea <eduardoquea3@gmail.com>
2024-09-20 20:07:31 -0500

    feat:default php file in intelephense

--- ftplugin/php.lua
+++ ftplugin/php.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<c-x>", "I//<esc>", { desc = "Comment line php" })
 vim.keymap.set("n", "<c-z>", "I<esc>lvld<esc>", { desc = "Comment line php" })
+
+require("lspconfig").intelephense.setup {
+  root_dir = require("lspconfig").util.root_pattern "php",
+}
