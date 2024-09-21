38f029a4e6de4341e0f0c493ebb2b09fb416e89b
Eduardo Quea <eduardoquea3@gmail.com>
2024-09-20 20:08:48 -0500

    feat: add diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,13 +1,21 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  -- opts = true,
-  event = "User FileOpened",
-  config = function()
-    require("gitsigns").setup {
-      current_line_blame = true,
-      current_line_blame_opts = {
-      	delay = 200,
-      },
-    }
-  end,
+  {
+    "lewis6991/gitsigns.nvim",
+    -- opts = true,
+    event = "User FileOpened",
+    config = function()
+      require("gitsigns").setup {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 200,
+        },
+      }
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    -- event = "User FileOpened",
+    -- cmd
+    config = true,
+  },
 }
