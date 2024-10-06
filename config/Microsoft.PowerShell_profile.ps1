77f6a842cc8a54a65eb0175f0cf1c73f5da5c3ec
Eduardo Quea <eduardoquea3@gmail.com>
2024-10-05 20:32:08 -0500

    feat: add script for copy config files of ( starship and wezterm )

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,16 @@
-Invoke-Expression (&starship init powershell)
+# import extras of me
+. C:\Users\eduar\OneDrive\Documentos\PowerShell\alias.ps1
+
+# imports Modules
 Invoke-Expression (& { (zoxide init powershell | Out-String)})
+fnm env --use-on-cd | Out-String | Invoke-Expression
 Set-TerminalIconsIcon -FileExtension ".astro" -Glyph "nf-custom-astro"
 Set-TerminalIconsIcon -FileExtension ".http" -Glyph "nf-cod-globe"
 Set-TerminalIconsIcon -FileExtension ".luacheckrc" -Glyph "nf-seti-lua"
 
 
 Import-Module PSReadLine
+Import-Module git-aliases -DisableNameChecking
 Import-Module Terminal-Icons
 
 Set-PSReadLineOption -EditMode Emacs
@@ -13,19 +18,14 @@ Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-#Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineKeyHandler -Key "Ctrl+k" -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key "Ctrl+j" -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key "Ctrl+e" -Function Acceptline
 
 Set-Alias -Name which -Value where.exe
+Set-Alias -Name tree -Value tre.exe
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+$env:BAT_THEME="Catppuccin Mocha"
+
+Invoke-Expression (&starship init powershell)
