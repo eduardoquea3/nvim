a48c2be628e6e4be2744dd68e23976962f4035aa
Eduardo Quea <eduardoquea3@gmail.com>
2024-10-10 10:23:44 -0500

    feat: add path of java

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -3,6 +3,20 @@ return {
   ft = { "java" },
   config = function()
     require("java").setup()
-    require("lspconfig").jdtls.setup {}
+    require("lspconfig").jdtls.setup {
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-21",
+                path = "U:\\Applications\\Scoop\\apps\\openjdk\\21.0.2-13",
+                default = true,
+              },
+            },
+          },
+        },
+      },
+    }
   end,
 }
