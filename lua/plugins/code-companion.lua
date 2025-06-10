2d0340c6ac56a3e1c27ce52bb610605354ddd006
eduaradoquea3 <eduardoquea3@gmail.com>
2025-06-10 09:25:56 -0500

    refactor: remove supermaven and change keymap of copilot suggestion

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -5,7 +5,23 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
-    "github/copilot.vim",
+    {
+      "github/copilot.vim",
+      config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap("i", "<A-c>", 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
+        vim.g.copilot_filetypes = {
+          ["*"] = false,
+          ["markdown"] = true,
+          ["text"] = true,
+          ["html"] = true,
+          ["javascript"] = true,
+          ["typescript"] = true,
+          ["python"] = true,
+          ["lua"] = true,
+        }
+      end,
+    },
   },
   event = "User FileOpened",
   keys = {
