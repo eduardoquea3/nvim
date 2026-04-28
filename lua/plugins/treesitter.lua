d87ea33c6490f3939329a256fd89f6b8f15307de
eduardoquea3 <eduardoquea3@gmail.com>
2026-04-28 09:33:13 -0500

    feat(treesitter): add parsers for rust, python, typescript and more languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,6 +9,54 @@ return {
       require("nvim-treesitter").setup {
         install_dir = vim.fn.stdpath "data" .. "/site",
       }
+      require("nvim-treesitter").install {
+        "rust",
+        "javascript",
+        "zig",
+        "python",
+        "html",
+        "css",
+        "json",
+        "lua",
+        "toml",
+        "luadoc",
+        "typescript",
+        "tsx",
+        "astro",
+        "yaml",
+        "http",
+        "markdown",
+        "markdown_inline",
+        "xml",
+        "graphql",
+        "regex",
+        "prisma",
+      }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "lua",
+          "prima",
+          "rust",
+          "zig",
+          "python",
+          "html",
+          "css",
+          "javascript",
+          "json",
+          "toml",
+          "typescript",
+          "tsx",
+          "astro",
+          "yaml",
+          "http",
+          "markdown",
+          "xml",
+          "prisma",
+        },
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
       -- require("nvim-treesitter.configs").setup {
       --   ensure_installed = {
       --     "python",
